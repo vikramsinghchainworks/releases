c4148574bc4a563bfe062a350e2958a19e0d3f89
vikramsinghchainworks <46514011+vikramsinghchainworks@users.noreply.github.com>
2019-04-02 18:21:45 +0530

    Update devopsBuild.sh

--- devopsBuild.sh
+++ devopsBuild.sh
@@ -3,10 +3,12 @@ p2pVerion=$(cat release.json | jq '.components[0]."p2p-server"')
 pulseWebVersion=$(cat release.json | jq '.components[0]."pulse-web"')
 pulseApiVersion=$(cat release.json | jq '.components[0]."pulse-api"')
 
-p2pVerion=${p2pVerion:1:-1}
-pulseWebVersion=${pulseWebVersion:1:-1}
-pulseApiVersion=${pulseApiVersion:1:-1}
+p2pVerion=${p2pVerion | cut -c 2-6}
+pulseWebVersion=${pulseWebVersion | cut -c 2-6}
+pulseApiVersion=${pulseApiVersion | cut -c 2-6}
 
 echo $p2pVerion
 echo $pulseWebVersion
 echo $pulseApiVersion
+#BAZ=${$(basename $FOO):0:1} # result is bad substitution
+#BAZ=$(_TMP=$(basename $FOO);${_TMP:0:1}) # this works
