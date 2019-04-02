920099f313d5b455ad502f4697a6a041e69c147e
vikramsinghchainworks <46514011+vikramsinghchainworks@users.noreply.github.com>
2019-04-02 17:27:33 +0530

    Update devopsBuild.sh

--- devopsBuild.sh
+++ devopsBuild.sh
@@ -2,6 +2,7 @@
 p2pVerion=$(cat release.json | jq '.components[0]."p2p-server"')
 pulseWebVersion=$(cat release.json | jq '.components[0]."pulse-web"')
 pulseApiVersion=$(cat release.json | jq '.components[0]."pulse-api"')
+
 p2pVerion="${p2pVerion:1: -1}"
 pulseWebVersion="${pulseWebVersion:1: -1}"
 pulseApiVersion="${pulseApiVersion:1: -1}"
